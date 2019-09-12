1c1edcbcb7801b89c9e5b1f39367495b5969d3b6
William B Messer <wmesser@clemson.edu>
2019-09-12 11:56:32 -0400

    MAKE GIT

--- hw1.c
+++ hw1.c
@@ -13,6 +13,7 @@ int main (int argc, char * argv[]) {
   int sendCount = 1;
   int receiveCount = 1;
 
+  MPI_Status status;
   int id, numprocs;
   int ierr = MPI_Init(&argc, &argv);
   ierr = MPI_Comm_rank(MPI_COMM_WORLD, &id);
