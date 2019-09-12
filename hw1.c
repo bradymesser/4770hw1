a90380993f3c93c3e946d39214493a0a6bbf5157
William B Messer <wmesser@clemson.edu>
2019-09-12 12:03:19 -0400

    MAKE GIT

--- hw1.c
+++ hw1.c
@@ -12,18 +12,18 @@ int main (int argc, char * argv[]) {
   int rank, numtasks;
   int sendCount = 1;
   int receiveCount = 1;
+  int send_tag = 69;
 
   MPI_Status status;
   int id, numprocs;
   int ierr = MPI_Init(&argc, &argv);
   ierr = MPI_Comm_rank(MPI_COMM_WORLD, &id);
   ierr = MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-  printf("ID %d\nnumprocs %d\n", id, numprocs);
   samples = atoi(argv[1]);
   if (id == 0) {
     samples = samples / numprocs;
     for (int i = 1; i < numprocs; i++) {
-      ierr = MPI_Send(&samples, sendCount, MPI_INT,i, MPI_ANY_TAG, MPI_COMM_WORLD);
+      ierr = MPI_Send(&samples, sendCount, MPI_INT,i, send_tag, MPI_COMM_WORLD);
     }
   } else {
       ierr = MPI_Recv(&samples, receiveCount, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
@@ -39,7 +39,7 @@ int main (int argc, char * argv[]) {
   }
 
   if (id != 0) {
-    ierr = MPI_Send(&count, sendCount, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD);
+    ierr = MPI_Send(&count, sendCount, MPI_INT, 0, send_tag, MPI_COMM_WORLD);
   } else {
     ierr = MPI_Recv(&count, numprocs, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
     pi = 4.0 * (double)count/(double)samples;
