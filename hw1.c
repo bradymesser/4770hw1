dd2580f79b7b47819024cd930143d556f7bcd2d0
William B Messer <wmesser@clemson.edu>
2019-09-12 12:26:52 -0400

    MAKE GIT

--- hw1.c
+++ hw1.c
@@ -38,19 +38,23 @@ int main (int argc, char * argv[]) {
     }
   }
 
-  if (id != 0) {
-    ierr = MPI_Send(&count, sendCount, MPI_INT, 0, send_tag, MPI_COMM_WORLD);
-  } else {
-    int temp = 0;
-    for (int i = 1; i < numprocs; i++) {
-      ierr = MPI_Recv(&temp, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-      count += temp;
-    }
-    samples = samples * numprocs;
-    pi = 4.0 * (double)count/(double)samples;
-    printf("Count = %d, Samples = %d, Estimate of pi = %7.5f\n", count, samples, pi);
-  }
-
+  // if (id != 0) {
+  //   ierr = MPI_Send(&count, sendCount, MPI_INT, 0, send_tag, MPI_COMM_WORLD);
+  // } else {
+  //   int temp = 0;
+  //   for (int i = 1; i < numprocs; i++) {
+  //     ierr = MPI_Recv(&temp, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+  //     count += temp;
+  //   }
+  //   samples = samples * numprocs;I
+  //   pi = 4.0 * (double)count/(double)samples;
+  //   printf("Count = %d, Samples = %d, Estimate of pi = %7.5f\n", count, samples, pi);
+  // }
+  int globalCount = 0;
+  ierr = MPI_Reduce(&count, &globalCount, numprocs, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  samples = samples * numprocs;I
+  pi = 4.0 * (double)count/(double)samples;
+  printf("Count = %d, Samples = %d, Estimate of pi = %7.5f\n", globalCount, samples, pi);
   ierr = MPI_Finalize();
   return 0;
 }
