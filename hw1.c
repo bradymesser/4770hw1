ec77a147275009d5fbd13dcae4fa83868840bdb0
William B Messer <wmesser@clemson.edu>
2019-09-12 11:55:27 -0400

    MAKE GIT

--- hw1.c
+++ hw1.c
@@ -10,14 +10,23 @@ int main (int argc, char * argv[]) {
   int samples;
   double pi;
   int rank, numtasks;
+  int sendCount = 1;
+  int receiveCount = 1;
 
   int id, numprocs;
   int ierr = MPI_Init(&argc, &argv);
   ierr = MPI_Comm_rank(MPI_COMM_WORLD, &id);
   ierr = MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-  printf("ID %d\nnumprocs %d", id, numprocs);
+  printf("ID %d\nnumprocs %d\n", id, numprocs);
   samples = atoi(argv[1]);
-
+  if (id == 0) {
+    samples = samples / numprocs;
+    for (int i = 1; i < numprocs; i++) {
+      ierr = MPI_Send(&samples, sendCount, MPI_INT,i, 69, MPI_COMM_WORLD);
+    }
+  } else {
+      ierr = MPI_Recv(&samples, receiveCount, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+  }
   count = 0;
   for (i = 0; i < samples; i++) {
     x = (double) rand() / RAND_MAX;
